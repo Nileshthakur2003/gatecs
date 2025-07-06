ea4ffbba27a6ae68f604aeead3dcf3e3b11b90f9
Nilesh Thakur <nileshthakurbgp@gmail.com>
2025-07-06 17:56:24 +0530

    improved23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="/signup"
+            href="/auth/signup"
           >
             Start Preparing
           </a>
@@ -41,7 +41,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/resources"
+          href="/prep/study"
         >
           <Image
             aria-hidden
@@ -50,11 +50,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Study Materials
+          Study
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/mock-tests"
+          href="/prep/mock"
         >
           <Image
             aria-hidden
